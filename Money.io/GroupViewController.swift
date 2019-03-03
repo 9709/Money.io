862b0a9e253038d0b2c37e6154343efdbb1edaae
Matthew Chan <mat_t4@hotmail.com>
2019-03-02 17:26:22 -0800

    Created Group view controller.
    Added table view to bottom of view controller.
    Added owing amount label to top half of view controller.

--- Money.io/GroupViewController.swift
+++ Money.io/GroupViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GroupViewController.swift
 //  Money.io
 //
 //  Created by Matthew Chan on 2019-03-02.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GroupViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
+    
 
 
 }
-
