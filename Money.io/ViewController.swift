7acf5311c5a5e5f3db01e7d3e68f4a491af328e5
Matthew Chan <mat_t4@hotmail.com>
2019-03-02 17:08:07 -0800

    Initial Commit

---
+++ Money.io/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Money.io
+//
+//  Created by Matthew Chan on 2019-03-02.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Money.ioTests/Money_ioTests.swift
@@ -0,0 +1,34 @@
+//
+//  Money_ioTests.swift
+//  Money.ioTests
+//
+//  Created by Matthew Chan on 2019-03-02.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+import XCTest
+@testable import Money_io
+
+class Money_ioTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
